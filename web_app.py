b8b433f8e3227518fb5da9b3a4882814501065fd
LuisdeOdiaga <luisodiaga33@gmail.com>
2026-03-16 16:18:32 -0600

    Arquitectura corregida: if __name__ al ras

--- web_app.py
+++ web_app.py
@@ -66,7 +66,7 @@ def webhook():
         
     return "OK", 200
 
-            if __name__ == "__main__":
-                 port = int(os.environ.get("PORT", 8080))
-                 app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+     port = int(os.environ.get("PORT", 8080))
+     app.run(host="0.0.0.0", port=port)
 
