677baae8a8d9088b488baeea1837b16c8e52a5d6
LuisdeOdiaga <luisodiaga33@gmail.com>
2026-03-17 17:20:05 -0600

    Arquitectura 100% validada y alineada

--- web_app.py
+++ web_app.py
@@ -72,6 +72,8 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 8080))
-     app.run(host="0.0.0.0", port=port)
+     port = int(os.environ.get("PORT",
+ 8080))
+     app.run(host="0.0.0.0",
+ port=port)
 
