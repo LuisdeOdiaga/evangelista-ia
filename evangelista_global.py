16e282ab0b28be89d507768195ac536dcfffdb6c
LuisdeOdiaga <luisodiaga33@gmail.com>
2026-03-20 16:17:59 -0600

    Hotfix: Memoria de sesion restaurada con exito

--- evangelista_global.py
+++ evangelista_global.py
@@ -48,6 +48,9 @@ def obtener_vector(texto):
 # Memoria de pantalla (Streamlit)
 #if "---messages" not in st.session_state:
     st.session_state.messages = []
+# Memoria de pantalla (Streamlit)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
